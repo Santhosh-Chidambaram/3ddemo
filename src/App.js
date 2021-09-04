ed13b73474a5545a5970ccc199ef15be125993b9
Santhosh-Chidambaram <santhoshsmart111@gmail.com>
2021-09-04 18:07:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Suspense } from "react";
+import { useGLTF, OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import CharacterModel from "./CharacterModel";
+import "./App.css";
+import Model from "./Kick";
+import FemaleModel from "./Female";
+import OthersModel from "./Others";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        height: "100vh",
+        backgroundColor: "#fff",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Canvas
+        camera={{ fov: 45 }}
+        performance={{ min: 0.1 }}
+        style={{
+          height: "80vh",
+        }}
+      >
+        <spotLight intensity={0.3} position={[5, 20, 20]} />
+        <pointLight position={[10, 10, 10]} intensity={2} />
+        <Suspense fallback={null}>
+          <CharacterModel />
+        </Suspense>
+      </Canvas>
+      <Canvas
+        camera={{ fov: 45 }}
+        performance={{ min: 0.1 }}
+        style={{
+          height: "80vh",
+        }}
+      >
+        <pointLight position={[10, 10, 10]} intensity={2} />
+        <Suspense fallback={null}>
+          <FemaleModel />
+        </Suspense>
+      </Canvas>
+      <Canvas
+        camera={{ fov: 45 }}
+        performance={{ min: 0.1 }}
+        style={{
+          height: "80vh",
+        }}
+      >
+        <pointLight position={[10, 10, 10]} intensity={2} />
+        <Suspense fallback={null}>
+          <OthersModel />
+        </Suspense>
+      </Canvas>
     </div>
   );
 }
